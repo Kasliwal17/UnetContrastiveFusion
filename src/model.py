c7d0cffed5006e2660455ba39da49e1f8202116b
Aditya Kasliwal <96430522+Kasliwal17@users.noreply.github.com>
2023-02-07 19:11:35 +0530

    Update model.py

--- src/model.py
+++ src/model.py
@@ -162,8 +162,8 @@ class Unet(SegmentationModel):
         )
 
         self.decoder = UnetDecoder(
-            encoder_channels=self.encoder.out_channels*2,
-            decoder_channels=decoder_channels,
+            encoder_channels=self.encoder.out_channels,
+            decoder_channels=decoder_channels*2,
             n_blocks=encoder_depth,
             use_batchnorm=decoder_use_batchnorm,
             center=True if encoder_name.startswith("vgg") else False,
